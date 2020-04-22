d9fe7f3c53e1f6070a44af6090a406e8c22fd7bd
BekahHW <github email address>
2020-04-22 17:28:29 -0400

    Del instagram feed and update copyright info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,11 @@ import './Footer.css'
 
 export default () => (
   <div>
-    <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
-    </h2>
-    <br />
-    <InstagramFeed count="8" />
     <footer className="footer">
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <a href="https://bekahhw.github.io/">BekahHW</a>.
         </span>
       </div>
     </footer>
