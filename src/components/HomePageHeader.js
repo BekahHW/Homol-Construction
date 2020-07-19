a7cf32473193617571df42b38a5d00662ac42ebc
BekahHW <github email address>
2020-07-18 22:38:55 -0400

    Add homepageheader, and adjust styles'

--- src/components/HomePageHeader.js
+++ src/components/HomePageHeader.js
@@ -24,7 +24,7 @@ const PageHeader = ({
           size="cover"
         />
       )}
-      <div className="container relative">
+      <div className="container--background relative">
         <h1 className="PageHeader--Title">{title}</h1>
         {subtitle && (
           <Content className="PageHeader--Subtitle" src={subtitle} />
--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
-import PageHeader from '../components/PageHeader'
+import HomePageHeader from '../components/HomePageHeader'
 import Content from '../components/Content'
 import Layout from '../components/Layout'
 
 // Export Template for use in CMS preview
 export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
   <main className="Home">
-    <PageHeader
+    <HomePageHeader
       large
       title={title}
       subtitle={subtitle}
